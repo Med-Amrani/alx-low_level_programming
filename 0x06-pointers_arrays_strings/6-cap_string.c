4087ff59cf00d208035b945fc062a60079412f38
Med-Amrani <amranofficiel1@gmail.com>
2023-03-31 17:24:56 -0700

    main.h

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++
@@ -1,7 +0,0 @@
-6. Expect the best. Prepare for the worst. Capitalize on what comes
-
-Write a function that capitalizes all words of a string.
-
-Prototype: char *cap_string(char *);
-Separators of words: space, tabulation, new line, ,, ;, ., !, ?, ", (, ), {, and }
-
