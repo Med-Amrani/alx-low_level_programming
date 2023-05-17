81dfbfc574922f6fd6a8d96e0233b1c37064e320
Med-Amrani <amranofficiel1@gmail.com>
2023-05-17 10:48:45 -0700

    up_date

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,6 +15,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
+
 	fd = open(filename, O_WRONLY | O_APPEND);
 
 	if (fd == -1)
@@ -22,7 +23,8 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
-		for (nletters = 0; text_content[nletters]; nletters++)	;
+		for (nletters = 0; text_content[nletters]; nletters++)
+			;
 
 		rWr = write(fd, text_content, nletters);
 
